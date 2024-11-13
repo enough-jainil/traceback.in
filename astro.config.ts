205e6b662924da5da6376a426c2fa84d73cba7bd
Jainil Prajapati 🪐 <github-jainil@1209.in>
2024-11-13 08:29:26 +0530

    change in astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
 
 export default defineConfig({
   // used to generate images
   site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
+    // site: 'https://memoryview.in',
+    process.env.VERCEL_ENV === "production"
+      ? "https://memoryview.in/"
       : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
-  trailingSlash: 'ignore',
+        ? `https://${process.env.VERCEL_URL}/`
+        : // : "https://localhost:3000/",
+          "https://memoryview.in/",
+  trailingSlash: "ignore",
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
+      exclude: ["@resvg/resvg-js"],
     },
   },
 });
