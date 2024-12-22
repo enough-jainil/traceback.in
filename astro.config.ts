f68c9d9abdff435d7cf5aac14b1738056f72376d
Jainil Prajapati 🪐 <github-jainil@1209.in>
2024-12-22 09:39:00 +0530

    Refactor site configuration and navigation; add new blog post and image
    - Updated site URL in `astro.config.ts` from "memoryview.in" to "traceback.in".
    - Renamed navigation item from "Blog" to "Reviews" in `BaseNavigation.astro`.
    - Added new blog post titled "The Matrix (1999) Review" with relevant metadata and content.
    - Deleted outdated blog post "Mistral AI’s Le Chat" from the content directory.
    - Introduced a new image asset for the blog.
    - Updated homepage content to reflect the new site focus on movie reviews.

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,11 @@ import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 
 export default defineConfig({
-  // used to generate images
-  site:
-    // site: 'https://memoryview.in',
-    process.env.VERCEL_ENV === "production"
-      ? "https://memoryview.in/"
-      : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}/`
-        : // : "https://localhost:3000/",
-          "https://memoryview.in/",
+  site: process.env.VERCEL_ENV === "production"
+    ? "https://traceback.in/"
+    : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/`
+      : "https://traceback.in/",
   trailingSlash: "ignore",
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
